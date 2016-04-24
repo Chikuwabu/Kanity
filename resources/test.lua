43aef35648b215eab361daa3827ab75d0beedd52
otya128 <otya128@users.noreply.github.com>
2016-04-24 19:33:01 +0900

    試しにスプライトをアニメーション

--- resources/test.lua
+++ resources/test.lua
@@ -1,9 +1,14 @@
+
+local toriniku = newCharacter(IMG_Load("SPTest.png"), 20, 16, CHARACTER_SCANAXIS.X)
+local testsp = Sprite(toriniku, 0, 0, 0);
+local renderer = control:renderer()
+renderer:clear()
+renderer:addObject(spriteToDrawableObject(testsp))
 function yopi()
     print("left")
+	if testsp:isXAnimationStarted() then
+		return
+    end
+	testsp:move(10, 10, 16)
 end
 setLeftButtonEvent(yopi)
-
-test();
-tst = (newSprite(0, "SPTest.png", 20, 16))
-tst:move(20,3,1);
-tst:draw()
