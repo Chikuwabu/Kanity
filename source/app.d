e35f3e206533033f9d91cf7d562df12b0008efb1
otya128 <otya128@users.noreply.github.com>
2016-05-12 15:57:39 +0900

    mapeditorを分離

--- source/app.d
+++ source/app.d
@@ -1,10 +1,7 @@
 import std.stdio;
 import kanity.core;
-import kanity.mapeditor.editor;
 
 void main(){
-    auto editor = new Editor("editorconfig.json", "kanitylog.log");
-    editor.run();
 	auto engine = new Engine("kanityconfig.json", "kanitylog.log");
 	engine.run();
 	return;
